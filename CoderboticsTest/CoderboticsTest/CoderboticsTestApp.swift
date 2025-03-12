426069d2d7a94c9ef7873fb922efb46206c1f04e
Jorge Marrufo <jorgemarrufo@Jorges-MacBook-Pro.local>
2025-03-12 12:05:32 -0600

    code

---
+++ CoderboticsTest/CoderboticsTest/CoderboticsTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoderboticsTestApp.swift
+//  CoderboticsTest
+//
+//  Created by Jorge Marrufo on 12/03/25.
+//
+
+import SwiftUI
+
+@main
+struct CoderboticsTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
